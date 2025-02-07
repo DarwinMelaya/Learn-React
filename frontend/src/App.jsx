d8657ac22a2e24209c8b9c5ddea389a7d7ccfd59
DarwinMelaya <116789003+DarwinMelaya@users.noreply.github.com>
2025-02-07 22:24:44 +0800

    React Router v6: Static Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes, Link } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
 import RootLayout from "./shared/components/Layouts/RootLayout";
 
 const App = () => {
   return (
     <RootLayout>
-      <div>This is the content</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cars" element={null} />
+      </Routes>
     </RootLayout>
   );
 };
--- frontend/src/shared/components/Layouts/RootLayout.jsx
+++ frontend/src/shared/components/Layouts/RootLayout.jsx
@@ -1,9 +1,13 @@
 import Footer from "../Partials/Footer";
 import Header from "../Partials/Header";
+import { Link } from "react-router-dom";
 
 const RootLayout = ({ children }) => {
   return (
     <div className="root-main">
+      <Link to="/">Go to Home</Link>
+      <Link to="/about">Go to About</Link>
+
       <Header />
       {children}
       <Footer />
